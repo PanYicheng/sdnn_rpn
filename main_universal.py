6c4da4ddd8117e8badec7c834f5fbb1460f4dd6a
PanYicheng <1316699379@qq.com>
2018-10-04 14:27:09 +0800

    refract main to use model definition

---
+++ main_universal.py
@@ -0,0 +1,99 @@
+"""
+__author__ = Nicolas Perez-Nieves
+__email__ = nicolas.perez14@imperial.ac.uk
+
+SDNN Implementation based on Kheradpisheh, S.R., et al. 'STDP-based spiking deep neural networks 
+for object recognition'. arXiv:1611.01421v1 (Nov, 2016)
+"""
+
+from SDNN_cuda import SDNN
+from Classifier import Classifier
+from model_def import models
+import numpy as np
+from os.path import dirname, realpath
+from math import floor
+import os
+import re
+import sys
+
+import time
+
+
+def main(model_name):
+    if models[model_name] == None:
+        print('No such model[%s] in model_def, exiting' % model_name)
+        return
+    # Flags
+    learn_SDNN = False  # This flag toggles between Learning STDP and classify features
+                        # or just classify by loading pretrained weights for the face/motor dataset
+    if learn_SDNN:
+        set_weights = True  # Loads the weights from a path (path_set_weigths) and prevents any SDNN learning
+        save_weights = True  # Saves the weights in a path (path_save_weigths)
+        save_features = True  # Saves the features and labels in the specified path (path_features)
+    else:
+        set_weights = True  # Loads the weights from a path (path_set_weigths) and prevents any SDNN learning
+        save_weights = False  # Saves the weights in a path (path_save_weigths)
+        save_features = True  # Saves the features and labels in the specified path (path_features)
+
+    # Results directories
+    path_set_weigths = models[model_name]['output_path']
+    path_save_weigths = models[model_name]['output_path']
+    path_features = models[model_name]['output_path']
+
+    # Create network
+    first_net = SDNN(models[model_name]['network_params'], models[model_name]['weight_params'],
+                     models[model_name]['stdp_params'], models[model_name]['total_time'],
+                     DoG_params=models[model_name]['DoG_params'],
+                     spike_times_learn=models[model_name]['learn_path'],
+                     spike_times_train=models[model_name]['train_path'],
+                     spike_times_test=models[model_name]['test_path'], device='GPU')
+
+    # Set the weights or learn STDP
+    if set_weights:
+        network_layer_num = len(models[model_name]['network_params'])
+        weight_path_list = [path_set_weigths + 'weight_' + str(i) + '.npy' for i in range(network_layer_num - 1)]
+        first_net.set_weights(weight_path_list)
+    else:
+        first_net.train_SDNN()
+
+    # Save the weights
+    if save_weights:
+        weights = first_net.get_weights()
+        for i in range(len(weights)):
+            np.save(path_save_weigths + 'weight_'+str(i), weights[i])
+
+    # Get features
+    X_train, y_train = first_net.train_features()
+    X_test, y_test = first_net.test_features()
+
+    # Save X_train and X_test
+    if save_features:
+        np.save(path_features + 'X_train', X_train)
+        np.save(path_features + 'y_train', y_train)
+        np.save(path_features + 'X_test', X_test)
+        np.save(path_features + 'y_test', y_test)
+
+    # ------------------------------- Classify -------------------------------#
+    classifier_params = {'C': 1.2, 'gamma': 'auto'}
+    train_mean = np.mean(X_train, axis=0)
+    train_std = np.std(X_train, axis=0)
+    X_train -= train_mean
+    X_test -= train_mean
+    X_train /= (train_std + 1e-5)
+    X_test /= (train_std + 1e-5)
+    svm = Classifier(X_train, y_train, X_test, y_test, classifier_params, classifier_type='SVM')
+    train_score, test_score = svm.run_classiffier()
+    print('Train Score: ' + str(train_score))
+    print('Test Score: ' + str(test_score))
+
+    print('DONE')
+
+
+if __name__ == '__main__':
+    start = time.time()
+    model_name = 'default'
+    if len(sys.argv) > 1:
+        model_name = sys.argv[1]
+    main(model_name)
+    end = time.time()
+    print('Time Userd', end-start)
---
+++ model_def.py
@@ -0,0 +1,57 @@
+"""
+This is the file which defines the models that
+will be used in the main_universal.py
+
+Each model contains:
+    network parameter
+    weight initialize parameter
+    stdp parameter
+    simulation time
+    DoG parameter
+    train test data paths
+    output weights paths if needed
+"""
+import math
+import numpy as np
+
+models = {}
+# ---------------------------------------------------default start
+model_name = 'default'
+DoG_params = {'img_size': (256, 160), 'DoG_size': 7, 'std1': 1., 'std2': 2.}  # img_size is (col size, row size)
+total_time = 15
+network_params = [{'Type': 'input', 'num_filters': 1, 'pad': (0, 0), 'H_layer': DoG_params['img_size'][1],
+                       'W_layer': DoG_params['img_size'][0]},
+                  {'Type': 'conv', 'num_filters': 4, 'filter_size': 5, 'th': 10.},
+                  {'Type': 'pool', 'num_filters': 4, 'filter_size': 7, 'th': 0., 'stride': 6},
+                  {'Type': 'conv', 'num_filters': 20, 'filter_size': 17, 'th': 60.},
+                  {'Type': 'pool', 'num_filters': 20, 'filter_size': 5, 'th': 0., 'stride': 5},
+                  {'Type': 'conv', 'num_filters': 20, 'filter_size': 5, 'th': 2.}]
+weight_params = {'mean': 0.8, 'std': 0.01}
+max_learn_iter = [0, 3000, 0, 5000, 0, 5000]
+stdp_params = {'max_learn_iter': max_learn_iter,
+                   'stdp_per_layer': [0, 10, 0, 5, 0, 1],
+                   'max_iter': sum(max_learn_iter),
+                   'a_minus': np.array([0, .003, 0, .0003, 0, .0003], dtype=np.float32),
+                   'a_plus': np.array([0, .004, 0, .0004, 0, .0004], dtype=np.float32),
+                   'offset_STDP': [0,
+                                   math.floor(network_params[1]['filter_size']),
+                                   0,
+                                   math.floor(network_params[3]['filter_size']/8),
+                                   0,
+                                   math.floor(network_params[5]['filter_size'])]
+                   }
+path = '.'
+spike_times_learn = [path + '/datasets/LearningSet/Face/', path + '/datasets/LearningSet/Motor/']
+spike_times_train = [path + '/datasets/TrainingSet/Face/', path + '/datasets/TrainingSet/Motor/']
+spike_times_test = [path + '/datasets/TestingSet/Face/', path + '/datasets/TestingSet/Motor/']
+output_path = 'results'
+models.update({model_name:{'DoG_params':DoG_params,
+                           'total_time':total_time,
+                           'network_params':network_params,
+                           'weight_params':weight_params,
+                           'stdp_params':stdp_params,
+                           'learn_path':spike_times_learn,
+                           'train_path':spike_times_train,
+                           'test_path':spike_times_test,
+                           'output_path':output_path}})
+# ---------------------------------------------------default end-------------------------------------------------------
