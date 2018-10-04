abdd92a4844286d02627b60be9f8d906099027d8
PanYicheng <1316699379@qq.com>
2018-10-04 13:32:12 +0800

    use cross validation on features

--- classifier_test.py
+++ classifier_test.py
@@ -2,10 +2,23 @@ from sklearn import datasets
 from Classifier import Classifier
 from matplotlib import pyplot as plt
 import numpy as np
+import os
+
+
+def load_features(dir_path):
+    file_names = ['X_train.npy', 'y_train.npy', 'X_test.npy', 'y_test.npy']
+    ret = [None, None, None, None]
+    for file_name in file_names:
+        path = os.path.join(dir_path, file_name)
+        if os.path.exists(path):
+            ret[0] = np.load(path)
+    return ret
+
 
 if __name__ == "__main__":
-    iris = datasets.load_iris()
-    svm = Classifier(iris.data, iris.target,iris.data, iris.target, {'C':1.0})
+    # iris = datasets.load_iris()
+    features = load_features('./results')
+    svm = Classifier(features[0], features[1], features[2], features[3], {'C':1.0})
     svm.cross_val_svm({'C':np.arange(1,101).tolist()}, 3)
     # print(svm.cvs_mean, svm.cvs_std)
     plt.show(svm.plots[-1])
